27401d0500cae740b84ddb419b61e5aa3843baf7
Andrew A Lee <andrew@andrewleedev.com>
2026-05-21 20:41:42 -0700

    Fixes code formatting

--- js/chessboard.js
+++ js/chessboard.js
@@ -1,20 +1,17 @@
-export default {
-	draw,
-	highlight
-};
-
-
-// ****************************
-
 function draw(boardEl) {
-	// TODO: draw the chessboard, 8 rows (divs)
-	// of 8 tiles (divs) each, inserting all DOM
-	// elements into `boardEl` div
+  // TODO: draw the chessboard, 8 rows (divs)
+  // of 8 tiles (divs) each, inserting all DOM
+  // elements into `boardEl` div
 }
 
 function highlight(tileEl) {
-	// TODO: clear previous highlights (if any) and
-	// then find the tiles in the two diagonals
-	// (major and minor) that `tileEl` belongs to,
-	// to highlight them via CSS class "highlighted"
+  // TODO: clear previous highlights (if any) and
+  // then find the tiles in the two diagonals
+  // (major and minor) that `tileEl` belongs to,
+  // to highlight them via CSS class "highlighted"
 }
+
+export default {
+  draw,
+  highlight
+};
