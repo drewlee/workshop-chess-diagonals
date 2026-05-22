b878cb0cb96496dfb3bbb69b7482734b698b5c29
Andrew A Lee <andrew@andrewleedev.com>
2026-05-21 20:41:42 -0700

    Fixes code formatting

--- js/app.js
+++ js/app.js
@@ -1,32 +1,26 @@
-import Chessboard from "./chessboard.js";
+import Chessboard from './chessboard.js';
 
+document.addEventListener('DOMContentLoaded', async function ready() {
+  const boardEl = document.getElementById('board');
+  const clearHighlightsBtn = document.getElementById('clear-highlights-btn');
 
-document.addEventListener("DOMContentLoaded",async function ready(){
-	var boardEl = document.getElementById("board");
-	var clearHighlightsBtn = document.getElementById("clear-highlights-btn");
+  function onClickTile(evt) {
+    const clickedEl = evt.target;
 
-	Chessboard.draw(boardEl);
+    // clicked on a board tile?
+    if (clickedEl.matches('#board > div > div')) {
+      Chessboard.highlight(clickedEl);
+    } else {
+      clearHighlights();
+    }
+  }
 
-	boardEl.addEventListener("click",onClickTile,false);
-	clearHighlightsBtn.addEventListener("click",clearHighlights,false);
+  function clearHighlights() {
+    Chessboard.highlight();
+  }
 
+  Chessboard.draw(boardEl);
 
-	// ********************************
-
-	function onClickTile(evt) {
-		var clickedEl = evt.target;
-
-		// clicked on a board tile?
-		if (clickedEl.matches("#board > div > div")) {
-			Chessboard.highlight(clickedEl);
-		}
-		else {
-			clearHighlights();
-		}
-	}
-
-	function clearHighlights() {
-		Chessboard.highlight();
-	}
-
+  boardEl.addEventListener('click', onClickTile, false);
+  clearHighlightsBtn.addEventListener('click', clearHighlights, false);
 });
